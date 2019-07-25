2b56497040894d5931259eeb3d239be03afc2495
mustafiz <mustafizmamun09@gmail.com>
2019-07-25 12:26:25 +1000

    safi dates file d

--- scripts/Data_Downloads.R
+++ scripts/Data_Downloads.R
@@ -1,1 +1,12 @@
 #Data_Downloads
+
+
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_clean.csv", 
+              destfile = "./data/SAFI_clean.csv", method="libcurl")
+
+
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_dates.xlsx", 
+              destfile = "./data/SAFI_dates.xlsx", method="libcurl")
+
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_dates.xlsx", 
+              destfile = "./data/SAFI_dates.xlsx", method="libcurl", mode="wb")
